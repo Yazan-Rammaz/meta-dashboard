17550029efb93b1da53be9d6d71463b2e35114e4
Yazan Ad <yazan.adnof@ramaaz.com>
2026-03-08 15:20:29 +0100

    cloudflare

--- src/app/api/auth/logout/route.ts
+++ src/app/api/auth/logout/route.ts
@@ -1,7 +1,7 @@
 import { cookies } from 'next/headers';
 import { NextResponse } from 'next/server';
 
-// This API route handles user logout by deleting the access token cookie.
+// This API route handles user logout by deleting the access token cookie
 export async function POST() {
     const cookieStore = await cookies();
     cookieStore.delete('access_token');
