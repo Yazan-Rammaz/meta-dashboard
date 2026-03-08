907fa7c2bff0cac4930ba3f9678d0496f7faa63e
Yazan Ad <yazan.adnof@ramaaz.com>
2026-03-08 16:51:08 +0100

    trailingSlash: false,

--- next.config.js
+++ next.config.js
@@ -9,6 +9,7 @@ const nextConfig = {
             },
         ],
     },
+    trailingSlash: false,
     env: {
         NEXT_PUBLIC_BASE_URL: process.env.NEXT_PUBLIC_BASE_URL,
         NEXT_PUBLIC_CLOUD_URL: process.env.NEXT_PUBLIC_CLOUD_URL,
