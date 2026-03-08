1eb851141e70839a1bf268c9274d8aba1f0ddb89
Yazan Ad <yazan.adnof@ramaaz.com>
2026-03-08 16:22:43 +0100

    new config

--- next.config.js
+++ next.config.js
@@ -1,42 +1,45 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "standalone",
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'res.cloudinary.com',
-      },
-    ],
-  },
-  env: {
-    NEXT_PUBLIC_BASE_URL: process.env.NEXT_PUBLIC_BASE_URL,
-    NEXT_PUBLIC_CLOUD_URL: process.env.NEXT_PUBLIC_CLOUD_URL,
-    NEXT_PUBLIC_BRAND_ICON_PATH: process.env.NEXT_PUBLIC_BRAND_ICON_PATH,
-    NEXT_PUBLIC_CATEGORY_ICON_PATH: process.env.NEXT_PUBLIC_CATEGORY_ICON_PATH,
-    NEXT_PUBLIC_BOUTIQUE_ICON_PATH: process.env.NEXT_PUBLIC_BOUTIQUE_ICON_PATH,
-    NEXT_PUBLIC_BOUTIQUE_BANNERS_PATH: process.env.NEXT_PUBLIC_BOUTIQUE_BANNERS_PATH,
-  },
-  // Disable source maps in production to prevent 404 errors for map files
-  productionBrowserSourceMaps: false,
-  reactStrictMode: true,
-  // Add rewrites to handle client-side routing
-  async rewrites() {
-    return [
-      {
-        source: '/dashboards/:path*',
-        destination: '/',
-      },
-      {
-        source: '/login',
-        destination: '/',
-      },
-      {
-        source: '/overview',
-        destination: '/',
-      }
-    ];
-  },
-}
+    output: 'standalone',
+    experimental: {
+        nodeMiddleware: true,
+    },
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'res.cloudinary.com',
+            },
+        ],
+    },
+    env: {
+        NEXT_PUBLIC_BASE_URL: process.env.NEXT_PUBLIC_BASE_URL,
+        NEXT_PUBLIC_CLOUD_URL: process.env.NEXT_PUBLIC_CLOUD_URL,
+        NEXT_PUBLIC_BRAND_ICON_PATH: process.env.NEXT_PUBLIC_BRAND_ICON_PATH,
+        NEXT_PUBLIC_CATEGORY_ICON_PATH: process.env.NEXT_PUBLIC_CATEGORY_ICON_PATH,
+        NEXT_PUBLIC_BOUTIQUE_ICON_PATH: process.env.NEXT_PUBLIC_BOUTIQUE_ICON_PATH,
+        NEXT_PUBLIC_BOUTIQUE_BANNERS_PATH: process.env.NEXT_PUBLIC_BOUTIQUE_BANNERS_PATH,
+    },
+    // Disable source maps in production to prevent 404 errors for map files
+    productionBrowserSourceMaps: false,
+    reactStrictMode: true,
+    // Add rewrites to handle client-side routing
+    async rewrites() {
+        return [
+            {
+                source: '/dashboards/:path*',
+                destination: '/',
+            },
+            {
+                source: '/login',
+                destination: '/',
+            },
+            {
+                source: '/overview',
+                destination: '/',
+            },
+        ];
+    },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
